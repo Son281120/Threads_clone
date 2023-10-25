5d075976f59d6fc569b20232743b48ff40b1f19c
Son281120 <changngoc10x28@gmail.com>
2023-10-25 17:59:48 +0700

    add git

---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,16 @@
+interface Props {
+    currentUserId: string;
+    accountId: string;
+    accountType: string;
+}
+
+const ThreadsTab = async ({ currentUserId, accountId, accountType}: Props) => {
+    //TODO: Fetch Profile threads
+    return (
+        <section>
+            ThreadsTab
+        </section>
+    )
+}
+
+export default ThreadsTab;
